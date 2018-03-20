358c38d662578246a1f9aa2d378aa3efb860f1ab
Simon Atkins <this-is-simon@github.com>
2018-03-20 17:00:29 +0000

    fixed horrible comma bug

--- console.rb
+++ console.rb
@@ -1,7 +1,20 @@
 require('pry')
 require_relative('models/bounty')
 
-bounty1 = Bounty.new({'name' => 'Garthak the Destroyer', 'species' => 'Reptowombat', 'value' => '1000', 'danger_level' => 'ermagerd'})
+options_hash1 = {'name' => 'Garthak the Destroyer', 'species' => 'Reptowombat', 'value' => '1000', 'danger_level' => 'ermagerd'}
+
+options_hash2 = {
+  'name' => 'Nebuchadnezzar',
+  'species' => 'Humanoid',
+  'value' => '2000',
+  'danger_level' => 'low'
+}
+
+bounty1 = Bounty.new(options_hash1)
+bounty2 = Bounty.new(options_hash2)
+
+binding.pry
+# Bounty.delete_all()
 
 bounty1.save()
-p bounty1
+bounty2.save()
--- models/bounty.rb
+++ models/bounty.rb
@@ -1,23 +1,36 @@
+require('pg')
+require('pry')
+
 class Bounty
+
   attr_reader :name, :species, :value, :danger_level
 
   def initialize( options )
-    @name = options['name'],
-    @species = options['species'],
-    @value = options['value'].to_i,
+    @species = options['species']
+    @value = options['value'].to_i
     @danger_level = options['danger_level']
+    @name = options['name']
   end
 
   def save
+    db = PG.connect( { dbname: 'bounty_hunter', host: 'localhost'} )
+
     sql = "INSERT INTO bounties (
     name, species, value, danger_level
   )
-  VALUES (
-    $1, $2, $3, $4
-  )
+  VALUES ($1, $2, $3, $4)
+  ;
   "
   values = [@name, @species, @value, @danger_level]
-  ;
+  db.prepare("save_it", sql)
+  db.exec_prepared("save_it", values)
+
+  db.close
+
   end
+
+  # def Bounty.delete_all
+  #   db = PG.connect
+  # end
 
 end
